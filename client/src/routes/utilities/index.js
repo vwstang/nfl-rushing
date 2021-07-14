96a04631024de7d016d90739a00390dc41566cf0
vwstang <vwstang6@gmail.com>
2021-07-14 01:05:27 -0400

    Dockerize the whole app

--- client/src/routes/utilities/index.js
+++ client/src/routes/utilities/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const express = require("express");
 
+const apiUrl = process.env.API_URL || "http://localhost:3000";
+
 const utilities = express.Router();
 
 utilities.get("/rushingstats", async (req, res) => {
@@ -10,16 +12,12 @@ utilities.get("/rushingstats", async (req, res) => {
       ...(req.headers.name && { "r-filter-name": req.headers.name }),
       ...(req.headers.sortby && { "r-sort-by": req.headers.sortby }),
       ...(req.headers.sortorder && { "r-sort-order": req.headers.sortorder }),
-      // ...(req.headers.yds && { "r-sort-yds": req.headers.yds }),
-      // ...(req.headers.lng && { "r-sort-lng": req.headers.lng }),
-      // ...(req.headers.td && { "r-sort-td": req.headers.td }),
       ...(req.headers.page && { "r-page": req.headers.page }),
       ...(req.headers.countppg && { "r-page-count": req.headers.countppg })
     };
-    const response = await axios.get(
-      "http://localhost:3000/api/stats/rushing",
-      { headers }
-    );
+    const response = await axios.get(`${apiUrl}/api/stats/rushing`, {
+      headers
+    });
     return res.send(response.data);
   } catch (error) {
     console.error("[routes.utilities.get.rushingstats]", error);
@@ -33,14 +31,10 @@ utilities.get("/rushingstats/getcsv", async (req, res) => {
       ...(req.headers.name && { "r-filter-name": req.headers.name }),
       ...(req.headers.sortby && { "r-sort-by": req.headers.sortby }),
       ...(req.headers.sortorder && { "r-sort-order": req.headers.sortorder })
-      // ...(req.headers.yds && { "r-sort-yds": req.headers.yds }),
-      // ...(req.headers.lng && { "r-sort-lng": req.headers.lng }),
-      // ...(req.headers.td && { "r-sort-td": req.headers.td })
     };
-    const response = await axios.get(
-      "http://localhost:3000/api/stats/rushing/csvdata",
-      { headers }
-    );
+    const response = await axios.get(`${apiUrl}/api/stats/rushing/csvdata`, {
+      headers
+    });
     return res.attachment("rushing.csv").send(response.data);
   } catch (error) {
     console.error("[routes.utilities.get.rushingstats.getcsv]", error);
--- server/src/controllers/db/index.js
+++ server/src/controllers/db/index.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require("mongodb");
 
-const uri =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+// const uri =
+// "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const connect = async (dbName) => {
+  const uri = process.env.MONGO_URL || "mongodb://localhost:27017";
   try {
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
